3d53368a5960e47b8e2569a1e0b4a8b5a105c32c
Nuno Freire <greven.pt@gmail.com>
2021-01-17 19:36:16 +0000

    Update Threat Indicator

--- modules/unitframes/elements/threatindicator.lua
+++ modules/unitframes/elements/threatindicator.lua
@@ -18,69 +18,46 @@ local function element_UpdateConfig(self)
 	self._config = E:CopyTable(C.modules.unitframes.units[unit].threat, self._config)
 end
 
-local function element_Override(self, event, unit)
-  if unit ~= self.unit or not IsInGroup() then return end
-
-  local element = self.ThreatIndicator
-
-  if(element.PreUpdate) then element:PreUpdate(unit) end
-
-  local feedbackUnit = element.feedbackUnit
-  unit = unit or self.unit
-
-  local status
-  if UnitExists(unit) then
-		if feedbackUnit and feedbackUnit ~= unit and UnitExists(feedbackUnit) then
-			status = UnitThreatSituation(feedbackUnit, unit)
-		else
-			status = UnitThreatSituation(unit)
-		end
+local function element_PostUpdate(self, _, status)
+  if not IsInGroup() then
+    self:Hide()
+    self.glow:Hide()
+    return
   end
 
-  local r, g, b
-	if status and status > 0 then
-		r, g, b = unpack(self.colors.threat[status])
-
-		if element.SetVertexColor then
-			element:SetVertexColor(r, g, b)
-    end
-
-    element.glow:SetVertexColor(element:GetVertexColor())
-    element.glow:Show()
-
-		element:Show()
-	else
-    element:Hide()
-    element.glow:Hide()
+  if status and status == 0 then
+    self:SetVertexColor(E:GetRGB(C.colors.threat[1]))
+    self:Show()
   end
 
-  if(element.PostUpdate) then
-		return element:PostUpdate(unit, status, r, g, b)
-	end
-
-	-- if status and status == 0 then
-  --   self:SetVertexColor(E:GetRGB(C.colors.threat[1]))
-	-- 	self:Show()
-  -- end
-
-  -- if status and status > 0 then
-  --   self.glow:SetVertexColor(self:GetVertexColor())
-  --   self.glow:Show()
-  -- else
-  --   self.glow:Hide()
-  -- end
+  if status and status > 0 then
+    self.glow:SetVertexColor(self:GetVertexColor())
+    self.glow:Show()
+  else
+    self.glow:Hide()
+  end
 end
 
-local function frame_UpdateThreatIndicator(self)
+local function Update(self, event)
+  print(event)
+end
+
+local function frame_UpdateThreatIndicator(self, event)
 	local element = self.ThreatIndicator
 	element:UpdateConfig()
 
-	element.feedbackUnit = element._config.feedback_unit
+  element.feedbackUnit = element._config.feedback_unit
+
+  if event == "GROUP_JOINED" or event == "GROUP_LEFT" then
+    element:ForceUpdate()
+  end
 
 	if element._config.enabled and not self:IsElementEnabled("ThreatIndicator") then
-		self:EnableElement("ThreatIndicator")
+    self:EnableElement("ThreatIndicator")
 	elseif not element._config.enabled and self:IsElementEnabled("ThreatIndicator") then
-		self:DisableElement("ThreatIndicator")
+    self:UnregisterEvent('GROUP_JOINED')
+    self:UnregisterEvent('GROUP_LEFT')
+    self:DisableElement("ThreatIndicator")
 	end
 
 	if self:IsElementEnabled("ThreatIndicator") then
@@ -94,10 +71,13 @@ function UF:CreateThreatIndicator(frame, parent)
   local glow = E:CreateGlow(parent or frame, 8)
   element.glow = glow
 
-	element.Override = element_Override
+	element.PostUpdate = element_PostUpdate
 	element.UpdateConfig = element_UpdateConfig
 
   frame.UpdateThreatIndicator = frame_UpdateThreatIndicator
+
+  frame:RegisterEvent('GROUP_JOINED', frame_UpdateThreatIndicator, true)
+  frame:RegisterEvent('GROUP_LEFT', frame_UpdateThreatIndicator, true)
 
   frame.ThreatIndicator = element
 
