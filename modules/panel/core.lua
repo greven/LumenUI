d7bf30d28337fdea84699fa81c05b796914f9828
Nuno Freire <greven.pt@gmail.com>
2021-02-07 15:19:26 +0000

    Add frame visibility for panel

--- modules/panel/core.lua
+++ modules/panel/core.lua
@@ -9,44 +9,50 @@ local _G = getfenv(0)
 -- ---------------
 
 local isInit = false
+local Panel = CreateFrame("Frame", "LumPanel", _G.UIParent)
 
-local function frame_UpdateConfig(self)
+local function panel_UpdateConfig(self)
     self._config = E:CopyTable(C.modules.panel, self._config)
 end
 
-local function frame_UpdateSize(self)
+local function panel_UpdateSize(self)
     self:SetSize(self._config.width, self._config.height)
 end
 
-local function frame_UpdatePoints(self) E:SetPosition(self, self._config.point) end
+local function panel_UpdatePoints(self) E:SetPosition(self, self._config.point) end
 
 function M:IsInit() return isInit end
 
 function M:Init()
     if not isInit and C.modules.panel.enabled then
-        local frame = CreateFrame("Frame", "LumPanel", _G.UIParent)
-        frame:SetFrameLevel(5)
-        local bg = E:SetBackdrop(frame, 2, 0.95, nil, {
+        Panel:SetFrameLevel(5)
+
+        local bg = E:SetBackdrop(Panel, 2, 0.95, nil, {
             bgFile = C.media.textures.vertlines,
             tile = true,
             tileSize = 8
         })
         bg:SetBackdropColor(E:GetRGBA(C.global.backdrop.color))
 
-        frame.UpdateConfig = frame_UpdateConfig
-        frame.UpdateSize = frame_UpdateSize
-        frame.UpdatePoints = frame_UpdatePoints
+        Panel.UpdateConfig = panel_UpdateConfig
+        Panel.UpdateSize = panel_UpdateSize
+        Panel.UpdatePoints = panel_UpdatePoints
 
         isInit = true
 
-        self:Update(frame)
+        self:Update()
     end
 end
 
-function M:Update(frame)
+function M:Update()
     if isInit then
-        frame:UpdateConfig()
-        frame:UpdateSize()
-        frame:UpdatePoints()
+        Panel:UpdateConfig()
+        Panel:UpdateSize()
+        Panel:UpdatePoints()
+
+        if Panel._config.visibility then
+            RegisterAttributeDriver(Panel, "state-visibility",
+                                    Panel._config.visibility)
+        end
     end
 end
